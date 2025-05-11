fbc3fc88e2e1e0e1059e0b799dc7ce59fbe705f1
Pragateeshwaran <geniuspekka1808@gmail.com>
2025-05-11 19:22:50 +0530

    learnt letters

--- basics/12. letters.py
+++ basics/12. letters.py
@@ -2,7 +2,12 @@ from manim import *
 
 class TextIn4K(Scene):
     def construct(self):
-        text = Text("Pragateesh!", font_size=96, color=BLUE)
-        circle = Circle(radius=1, color=RED)
-        self.play(Transform(circle, text))
+        text1 = Text("Pragateesh!", font_size=96, color=BLUE)
+        text2 = Text("weds", font_size=96, color=RED)
+        text3 = Text("Kayal!", font_size=96, color=BLUE)
+        self.play(Write(text1))
+        self.wait(1)
+        self.play(Transform(text1, text2))
+        self.wait(1)
+        self.play(Transform(text2, text3))
         self.wait(2)
